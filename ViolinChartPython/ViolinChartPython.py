1b1d80746966cd26c849150fdcfaa1511e9aa6e8
nahidul.rimon <nahidulislam107gmail.com>
2017-11-13 18:13:53 +0600

    Initial Commit

--- ViolinChartPython/ViolinChartPython.py
+++ ViolinChartPython/ViolinChartPython.py
@@ -1,1 +1,21 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def show():
+    # Load the data
+    sns.set_style("whitegrid")
+
+    # Create violinplot
+    tips = sns.load_dataset("tips")
+
+    sns.violinplot(x="day", y="total_bill", data=tips)
+
+    # Show the plot
+    plt.show()
+
+
+
+show()
 
